0261e839f3445e965acbca099e0187fbd7bfa26a
felipe <martinsfelipegomes2005@gmail.com>
2025-10-29 11:48:47 -0300

    Implementando teste individual na main

--- app/Main.java
+++ app/Main.java
@@ -5,7 +5,20 @@ import strategy.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("=== TESTE COM ARITMÉTICA ===");
+        /*teste individual*/
+        MediaStrategy estrategia = new Aritmetica();
+        Disciplina d = new Disciplina(estrategia);
+
+        d.setNome("Padrões de Desenvolvimento");
+        d.setP1(10);
+        d.setP2(5);
+        d.calcularMedia();
+
+        System.out.printf("P1: %.2f  P2: %.2f  Média: %.2f  Situação: %s%n",
+                d.getP1(), d.getP2(), d.getMedia(), d.getSituacao());
+
+        /*teste com ambos*/
+        System.out.println("\n=== TESTE COM ARITMÉTICA ===");
         testarEstrategia(new Aritmetica());
 
         System.out.println("\n=== TESTE COM GEOMÉTRICA ===");
@@ -14,12 +27,12 @@ public class Main {
     }
 
     private static void testarEstrategia(MediaStrategy estrategia) {
-        Disciplina d = new Disciplina(estrategia);
-        d.setNome("Padrões de Desenvolvimento");
-        d.setP1(10);
-        d.setP2(5);
-        d.calcularMedia();
+        Disciplina e = new Disciplina(estrategia);
+        e.setNome("Padrões de Desenvolvimento");
+        e.setP1(10);
+        e.setP2(5);
+        e.calcularMedia();
         System.out.printf("P1: %.2f  P2: %.2f  Média: %.2f  Situação: %s%n",
-                d.getP1(), d.getP2(), d.getMedia(), d.getSituacao());
+                e.getP1(), e.getP2(), e.getMedia(), e.getSituacao());
     }
 }
